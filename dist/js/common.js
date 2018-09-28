40404d8381d0bd2c7bcd46196f1764fca9713a34
sergey09 <sergey.garmash09@gmail.com>
2018-09-28 16:50:50 +0300

    update

--- dist/js/common.js
+++ dist/js/common.js
@@ -3,88 +3,95 @@ $( document ).ready(function() {
 	// variable
 	var openMenu, openModal, closeModal, carousel, openModalQuestion, descriptionBlockHeader, moreTextDescription; 
 
-    $(function(){
-    	// open menu
-		openMenu = $('.toggle__nav');
-	 	openMenu.on('click' , function (e) {
-	        $(".active").slideToggle('slow');
-	    });
+	// open menu
+	openMenu = $('.toggle__nav');
+ 	openMenu.on('click' , function (e) {
+        $(".active").slideToggle('slow');
+    });
 
-	 	// open modal
-	 	openModal = $('.block__entry a');
-	    openModal.on('click', function (e) {
-		 	$('.modal').toggleClass('dBlock');
-		 	$(this).css({
-		 		background: '#f3f3f3',
-		 		color: '#999999',
-		 		cursor: 'default'
-		 	});
-		});
+ 	// open modal
+ 	openModal = $('.block__entry a');
+    openModal.on('click', function (e) {
+	 	$('.modal').toggleClass('dBlock');
+	 	$(this).css({
+	 		background: '#f3f3f3',
+	 		color: '#999999',
+	 		cursor: 'default'
+	 	});
+	});
 
-	    // close menu
-		closeModal = $('.close');
-		closeModal.on('click', function (e) {
-		 	$('.modal').removeClass('dBlock');
-		});
+    // close menu
+	closeModal = $('.close');
+	closeModal.on('click', function (e) {
+	 	$('.modal').removeClass('dBlock');
+	});
 
-		descriptionBlockHeader = $('.block__description');
-		descriptionBlockHeader.on('click','a', function (e) {
-	        event.preventDefault();
-	        var id  = $(this).attr('href'),
-	            top = $(id).offset().top;
-	        $('body,html').animate({scrollTop: top}, 1000);
-	    });
+	descriptionBlockHeader = $('.block__description');
+	descriptionBlockHeader.on('click','a', function (e) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 
-		// open modal
-		openModalQuestion = $('.btn__question');
-	 	openModalQuestion.on('click', function (e) {
-		 	$('.modal').toggleClass('dBlock');
-		});
+	// open modal
+	openModalQuestion = $('.btn__question');
+ 	openModalQuestion.on('click', function (e) {
+	 	$('.modal').toggleClass('dBlock');
+	});
 
-		moreTextDescription = $('.more__button');
-		moreTextDescription.on('click', function(e) {
-			$('.description__footer_text').toggleClass('active__text_descrition');
-		});
-	 	// slick slide
-		carousel = $('.reviews__carousel');
-		carousel.slick({
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  autoplay: false,
-		  autoplaySpeed: 2000,
-		  responsive: [{
-		  	breakpoint: 730,
-		  	settings: {
-		  		slidesToShow: 1,
-	        	slidesToScroll: 1,
-		  	}
-		  }]
-		});
+	moreTextDescription = $('.more__button');
+	moreTextDescription.on('click', function(e) {
+		$('.description__footer_text').toggleClass('active__text_descrition');
+	});
+ 	// slick slide
+	carousel = $('.reviews__carousel');
+	carousel.slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  autoplay: false,
+	  autoplaySpeed: 2000,
+	  responsive: [{
+	  	breakpoint: 730,
+	  	settings: {
+	  		slidesToShow: 1,
+        	slidesToScroll: 1,
+	  	}
+	  }]
+	});
 
-		//form validate
-		$('.modal__form').validate({
- 			rules: {
- 				name: {
- 					required : true,
- 					minlength: 3
- 				},
- 				email: {
- 					required : true,
- 					minlength: 5
- 				}
- 			},
- 			highlight: function(element, errorClass, validClass) {
-          		$(element).parents("div.form__input").addClass('form__error').removeClass('form__success');
-          		$(element).parents("div.form__input").addClass('form__error').removeClass('form__success');
-        	},
-	        // remove class
-	        unhighlight: function(element, errorClass, validClass) {
-	          $(element).parents("label.form__input").removeClass('form__error').addClass('form__success');
-	          $(element).parents("label.form__input").removeClass('form__error').addClass('form__success');
-	        },
-	        submitHandler: function() {
-	            alert('valid form')
-	        }
- 		})
+	//form validate
+	$('.modal__form').validate({
+		rules: {
+			name: {
+				required : true,
+				minlength: 3
+			},
+			email: {
+				required : true,
+				minlength: 5
+			},
+			tel: {
+				required : true
+			}
+		},
+		highlight: function(element, errorClass, validClass) {
+	  		$(element).parents("div.form__input").addClass('form__error').removeClass('form__success');
+	  		$(element).parents("div.form__input").addClass('form__error').removeClass('form__success');
+		},
+	    // remove class
+	    unhighlight: function(element, errorClass, validClass) {
+      		$(element).parents("label.form__input").removeClass('form__error').addClass('form__success');
+      		$(element).parents("label.form__input").removeClass('form__error').addClass('form__success');
+	    }
+	})
+
+ 	$('input[type=tel]').on('keyup keypress', function(e) {
+       if (e.keyCode == 8 || e.keyCode == 46) {}
+       else
+        {
+            var letters=' +-()1234567890';
+            return (letters.indexOf(String.fromCharCode(e.which))!=-1);
+        }
     });
 });
